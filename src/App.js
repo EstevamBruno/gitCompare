53e00b54f3331e72d86338f3fadc15effba3176b
Bruno Kennedy <brunokennedy16@gmail.com>
2020-03-24 20:51:45 -0300

    Aplicacao feita junto ao curso bootCamp: goReact

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GlobalStyled from  './styles/global'
+
+import Index from './pages/main/index'
+
+const App = () => (
+  <>
+    <GlobalStyled />
+    <Index />
+  </>
+);
+
+export default App;
